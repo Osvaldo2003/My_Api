a00aafab9afbd96f7e2c97cb459b7d636c7d0dfb
Osvaldo2003 <221258@ids.upchiapas.edu.mx.>
2024-07-11 12:57:38 -0600

    [UPDATE] dist deleted3

--- src/adapters/repositories/s3StorageRepository.ts
+++ src/adapters/repositories/s3StorageRepository.ts
@@ -5,13 +5,11 @@ export class S3StorageRepository implements IStorageRepository {
     private s3: AWS.S3;
 
     constructor() {
-        
         const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-        const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;  
-        const sessionToken = process.env.AWS_SESSION_TOKEN; 
+        const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+        const sessionToken = process.env.AWS_SESSION_TOKEN;
         const region = process.env.AWS_REGION;
 
-        
         if (!accessKeyId || !secretAccessKey || !region) {
             throw new Error('AWS credentials or region are not defined in .env');
         }
@@ -19,27 +17,26 @@ export class S3StorageRepository implements IStorageRepository {
         this.s3 = new AWS.S3({
             accessKeyId,
             secretAccessKey,
-            sessionToken: sessionToken || undefined, 
-            region, 
+            sessionToken: sessionToken || undefined,
+            region,
         });
     }
 
     async upload(file: Express.Multer.File): Promise<string> {
         const bucketName = process.env.AWS_S3_BUCKET_NAME;
-        
-        
+
         if (!bucketName) {
             throw new Error('AWS S3 bucket name is not defined in .env');
         }
 
         const params = {
             Bucket: bucketName,
-            Key: `${Date.now()}-${file.originalname}`, 
+            Key: `${Date.now()}-${file.originalname}`,
             Body: file.buffer,
         };
 
         const result = await this.s3.upload(params).promise();
-        
+
         return result.Location;
     }
 }
